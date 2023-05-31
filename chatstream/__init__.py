1cd208fb72e08bf520182260c5e0a93efd6706a1
riversun.org@gmail.com <riversun.org@gmail.com>
2023-05-31 09:39:56 +0900

    add init.py for preset prompt classes

--- chatstream/__init__.py
+++ chatstream/__init__.py
@@ -1,3 +1,8 @@
 from .chat_stream import ChatStream
 from .chat_prompt import AbstractChatPrompt
 from .request_handler.simple_session_request_handler import SimpleSessionRequestHandler
+
+# chat prompt presets
+
+from .chat_prompt_presets.chat_prompt_redpajama_incite import ChatPromptTogetherRedPajamaINCITEChat
+from .chat_prompt_presets.chat_prompt_rinna import ChatPromptRinnaJapaneseGPTNeoxInst
