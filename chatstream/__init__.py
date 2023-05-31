15ebe34637233ab45fe2dfa7e96464e995118880
riversun.org@gmail.com <riversun.org@gmail.com>
2023-05-31 18:16:22 +0900

    pull up public classes

--- chatstream/__init__.py
+++ chatstream/__init__.py
@@ -1,8 +1,10 @@
 from .chat_stream import ChatStream
 from .chat_prompt import AbstractChatPrompt
+from .request_handler.request_handler import AbstractRequestHandler
+
+# request handler presets
 from .request_handler.simple_session_request_handler import SimpleSessionRequestHandler
 
 # chat prompt presets
-
 from .chat_prompt_presets.chat_prompt_redpajama_incite import ChatPromptTogetherRedPajamaINCITEChat
 from .chat_prompt_presets.chat_prompt_rinna import ChatPromptRinnaJapaneseGPTNeoxInst
