a25bf85ca7cfda510b88ed33d73685c687ed517a
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-06 09:33:38 +0900

    resolve #6

--- chatstream/util_request_id.py
+++ chatstream/util_request_id.py
@@ -10,9 +10,16 @@ def set_req_id(request: Request):
     :param request:
     :return:
     """
-    if not hasattr(request.state, "__chatstream__"):
-        request.state.__chatstream__ = {}
-    request.state.__chatstream__["req_id"] = str(uuid.uuid4())
+    session_mgr = getattr(request.state, "session", None)
+    if session_mgr is not None:
+        req_id = session_mgr.get_session_id()[-4:]
+
+        if not hasattr(request.state, "__chatstream__"):
+            request.state.__chatstream__ = {}
+
+        request.state.__chatstream__["req_id"] = req_id
+    else:
+        request.state.__chatstream__["req_id"] = str(uuid.uuid4())[-4:]  # 下4桁
 
 
 def req_id(request: Request):
@@ -21,4 +28,4 @@ def req_id(request: Request):
     :param request:
     :return:
     """
-    return f"req_id_{request.state.__chatstream__.get('req_id', '0000')[-4:]}"  # 下4桁
+    return f"req_id_{request.state.__chatstream__['req_id']}"
