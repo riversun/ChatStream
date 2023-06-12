a185b699c287f8b093b7feb1abaf8cc5b7b1b31d
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-12 13:05:14 +0900

    add locale util

---
+++ chatstream/easy_locale.py
@@ -0,0 +1,88 @@
+import locale
+
+
+class EasyLocale:
+    """
+    A helper class to handle localization. It supports English and Japanese languages, and falls back to English
+    by default if the requested locale is not supported.
+    """
+
+    def __init__(self, opts=None):
+        """
+        Initialize an EasyLocale object.
+
+        :param opts: A dictionary of options for initialization. It can contain keys 'fallback_locale' and 'locale'.
+                     'fallback_locale' specifies the locale to be used if the requested locale is not supported.
+                     'locale' specifies the initial locale to be used.
+        """
+
+        if opts is None:
+            opts = {}
+        self.fallbackLocale = opts.get('fallback_locale', 'en')
+
+        current_locale = self._get_current_locale()
+
+        self.locale = current_locale
+
+        if opts.get('locale', None) is not None:
+            self.locale = opts.get('locale', None)
+
+    def _get_current_locale(self):
+        """
+        Private method to get the current locale of the system in ISO 639-1 format.
+        It supports only English and Japanese languages.
+
+        :return: The current locale of the system in ISO 639-1 format.
+        :rtype: str
+        """
+
+        raw_language = locale.getlocale()[0];
+        if '_' in raw_language:
+            raw_language = raw_language.split('_')[0]
+
+        current_locale = 'en'
+
+        if raw_language == 'Japanese':
+            current_locale = 'ja'
+        elif raw_language == 'English':
+            current_locale = 'en'
+
+        return current_locale
+
+    def get_locale(self):
+        """
+        Get the locale currently being used.
+
+        :return: The current locale.
+        :rtype: str
+        """
+
+        return self.locale
+
+    def to_str(self, message):
+        """
+        Convert a localized message to a string in the current locale.
+
+        :param message: The localized message. It can be a string or a dictionary containing locale-message pairs.
+        :return: The message in the current locale if it exists, otherwise the message in the fallback locale.
+                 If the message is neither a string nor a dictionary, it returns '(locale_type_error)'.
+        :rtype: str
+        """
+        user_locale = self.locale
+        out = ''
+
+        if isinstance(message, dict):
+            out = message.get(user_locale)
+            if not out:
+
+                if '-' in user_locale:
+                    language = user_locale.split('-')[0]
+                    out = message.get(language)
+                    if not out:
+                        out = message.get(self.fallbackLocale)
+            return out
+        elif isinstance(message, str):
+            return message
+        else:
+            return '(locale_type_error)'
+
---
+++ tests/test_easy_locale.py
@@ -0,0 +1,25 @@
+import pytest
+
+from chatstream.easy_locale import EasyLocale
+
+
+def test_easy_locale():
+    obj = EasyLocale({'fallbackLocale': 'en'})
+    greeting_message = {"ja": "こんにちは", "en": "Hello"}
+    assert obj.to_str(greeting_message) in ["こんにちは", "Hello"]
+
+    obj = EasyLocale({'fallbackLocale': 'en', 'locale': 'en'})
+    greeting_message = {"ja": "こんにちは", "en": "Hello"}
+    assert obj.to_str(greeting_message) == "Hello"
+
+    obj = EasyLocale({'fallbackLocale': 'en', 'locale': 'en-US'})
+    greeting_message = {"ja": "こんにちは", "en": "Hello"}
+    assert obj.to_str(greeting_message) == "Hello"
+
+    obj = EasyLocale({'fallbackLocale': 'en', 'locale': 'ja-JP'})
+    greeting_message = {"ja": "こんにちは", "en": "Hello"}
+    assert obj.to_str(greeting_message) == "こんにちは"
+
+    assert obj.to_str('おはよう') == 'おはよう'
+    obj = EasyLocale({'locale': None})
+    assert obj.to_str({"ja": "こんにちは", "en": "Hello"}) in ["こんにちは", "Hello"]
