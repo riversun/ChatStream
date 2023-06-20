e29df38c44f6325fa156d41d3f56088c57a971ce
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-20 17:21:37 +0900

    remove unneeded comments

--- tests/test_chat_prompt_of_rinna.py
+++ tests/test_chat_prompt_of_rinna.py
@@ -1,53 +1,7 @@
 import pytest
 
-# from chatstream import AbstractChatPrompt
-
 from chatstream import ChatPromptRinnaJapaneseGPTNeoxInst as ChatPrompt
 
-
-# class ChatPromptRinnaForTest(AbstractChatPrompt):
-#
-#     def __init__(self):
-#         super().__init__()  # Call the initialization of the base class
-#         self.set_requester("ユーザー")
-#         self.set_responder("システム")
-#
-#     def get_stop_strs(self):
-#         if not self.chat_mode:
-#             return None
-#         return []
-#
-#     def get_replacement_when_input(self):
-#         return [("\n", "<NL>")]
-#
-#     def get_replacement_when_output(self):
-#         return [("<NL>", "\n")]
-#
-#     def create_prompt(self, omit_last_message=False):
-#         if self.chat_mode == False:
-#             return self.get_requester_last_msg()
-#
-#         # Chat Mode == True の場合のプロンプトを構築する
-#         ret = self.system;
-#         for i, chat_content in enumerate(self.chat_contents):
-#             chat_content_role = chat_content.get_role()
-#             chat_content_message = chat_content.get_message()
-#
-#             if omit_last_message and (i == len(self.chat_contents) - 1):
-#                 # omit_last_messageが有効で、今が最後のメッセージの場合
-#                 chat_content_message = None  # 最後のメッセージパートは無効にする
-#
-#             if chat_content_role:
-#
-#                 if chat_content_message:
-#                     merged_message = chat_content_role + ": " + chat_content_message + "<NL>"
-#                 else:
-#                     merged_message = chat_content_role + ": "
-#
-#                 ret += merged_message
-#
-#         return ret
-#
 
 class TestChatPromptRinna:
 
