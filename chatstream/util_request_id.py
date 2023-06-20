32078cbb49d240f2ada646bb4d1f11a2ee9b12ed
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-20 17:20:30 +0900

    update id policy when session is None

--- chatstream/util_request_id.py
+++ chatstream/util_request_id.py
@@ -10,14 +10,18 @@ def req_id(request: Request):
     :return:
     """
     session_mgr = getattr(request.state, "session", None)
+
     if session_mgr is not None:
         req_id = session_mgr.get_session_id()[-4:]
         if not hasattr(request.state, "__chatstream__"):
             request.state.__chatstream__ = {}
         request.state.__chatstream__["req_id"] = req_id
+        return f"req_id_{request.state.__chatstream__['req_id']}"
     else:
+        # セッションが使えない場合
         if not hasattr(request.state, "__chatstream__"):
             request.state.__chatstream__ = {}
         request.state.__chatstream__["req_id"] = str(uuid.uuid4())[-4:]  # 下4桁
+        return f"req_id_NOSESSION_{request.state.__chatstream__['req_id']}"
 
-    return f"req_id_{request.state.__chatstream__['req_id']}"
+
